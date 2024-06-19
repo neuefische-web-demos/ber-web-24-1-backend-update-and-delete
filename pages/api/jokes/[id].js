25d97c18b09cbeccfc14b34e5243aa5a6ae7c618
Niklas Röttger <niklas.roettger@outlook.de>
2024-06-19 14:46:46 +0200

    add demo code

--- pages/api/jokes/[id].js
+++ pages/api/jokes/[id].js
@@ -14,4 +14,17 @@ export default async function handler(request, response) {
 
     response.status(200).json(joke);
   }
+
+  if (request.method === "PUT") {
+    const updatedJoke = request.body;
+    await Joke.findByIdAndUpdate(id, updatedJoke);
+
+    response.status(200).json({ status: "Joke updated!" });
+  }
+
+  if (request.method === "DELETE") {
+    await Joke.findByIdAndDelete(id);
+
+    response.status(200).json({ status: "Joke deleted succesfully!" });
+  }
 }
